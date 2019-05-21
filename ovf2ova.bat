d51a6e93f8f7096710ee28499026c13861e07cc3
virtualKJ <50858356+virtualKJ@users.noreply.github.com>
2019-05-21 10:55:51 -0400

    Create ovf2ova.bat

---
+++ ovf2ova.bat
@@ -0,0 +1,11 @@
+::With Direct inputs for source destination | tired of fucking with it
+for /R Z:\ %%i IN (*.vmx) DO ovftool.exe "%%i" "E:\out\%%~ni.ova"
+::
+::This should in theory work
+for /R %%1 %%i IN (*.vmx) DO ovftool.exe "%%i" "%%2%%~ni.ova"
+::
+::or with set
+::
+SET source=
+SET dest=
+for /R %%SOURCE% %%i IN (*.vmx) DO ovftool.exe "%%i" "%%DEST%%~ni.ova"
